4410a46ba25f5fd9f898a8308f70274686293f40
bruno-duart <53445564+bruno-duart@users.noreply.github.com>
2021-07-23 14:34:04 -0300

    Trabalho 2 - Finalizado

--- catkin_ws/src/interact_two_tb3/script/control_two_tb3.py
+++ catkin_ws/src/interact_two_tb3/script/control_two_tb3.py
@@ -3,19 +3,19 @@
 import rospy
 from geometry_msgs.msg import Twist
 
-class Control_two_tb3:
+class Control_two_tb3: # declaração da classe de controle
     def __init__(self):
-        rospy.init_node("control_two_tb3_node", anonymous=False)
-        rospy.Subscriber("/cmd_vel", Twist, self.update_vel)
-        
+        rospy.init_node("control_two_tb3_node", anonymous=False) 
+        rospy.Subscriber("/cmd_vel", Twist, self.update_vel) # tópico de escuta
+
+        # Tópicos de publicação        
         self.pub_tb3_1 = rospy.Publisher("tb3_0/cmd_vel", Twist, queue_size=10)
         self.pub_tb3_2 = rospy.Publisher("tb3_1/cmd_vel", Twist, queue_size=10)
 
         self.twist = Twist()
 
-    def update_vel(self, msg):
+    def update_vel(self, msg): # atualização de velocidades
         self.twist = msg
-        #rospy.loginfo(self.twist)
 
     def start(self):
         while not rospy.is_shutdown():
